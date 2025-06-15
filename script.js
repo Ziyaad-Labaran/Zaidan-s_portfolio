77371ef3d6ae8a3a85ff8f81624e63a7df30deb6
Ziyaad-Labaran <Ziyaad.zalabaran@gmail.com>
2025-06-15 12:42:27 +0000

    send

--- script.js
+++ script.js
@@ -29,4 +29,27 @@
         });
 
 
+const texts = ["Architectural drafter", "Graphic Designer"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+    if (count === texts.length) count = 0;
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById('typed-text').textContent = letter;
+
+    if (letter.length === currentText.length) {
+        setTimeout(() => {
+            index = 0;
+            count++;
+            type();
+        }, 2000);
+    } else {
+        setTimeout(type, 100);
+    }
+})();
         
